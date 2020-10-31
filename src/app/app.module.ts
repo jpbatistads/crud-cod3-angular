1d1e9c3636089fba9047494194f49eab1ce6dbb1
jpbatista <joao.batista@a.ficr.edu.br>
2020-10-30 22:30:41 -0300

    Adição da pasta view para criar as visualizações que representam as páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list'; 
+import { MatCardModule } from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component'; 
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import {MatListModule} from '@angular/material/list';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [ /*Qualque aplicação pode importar os módulos abaixo*/
     BrowserModule,
@@ -26,7 +29,8 @@ import {MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
