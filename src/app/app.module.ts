67e0344cb4401600a0a3f0495669a5549edaec5a
jpbatista <joao.batista@a.ficr.edu.br>
2020-11-07 14:57:26 -0300

    Criação do componetente criar produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'; 
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/products/product-create/product-create.component'; 
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    ProductCreateComponent
   ],
   imports: [ /*Qualque aplicação pode importar os módulos abaixo*/
     BrowserModule,
